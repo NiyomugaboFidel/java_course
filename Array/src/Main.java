5520a700e959a2186ffc4a86520481fb83ecd2cc
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2024-10-29 08:00:13 +0200

    add numbers

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         int[] numbers =  new int[5];
+         numbers[0] = 30;
+         numbers[1] =40;
+        System.out.println(Arrays.toString(numbers));
     }
 }
